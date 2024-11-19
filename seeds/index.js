da61316ebce9eedf7460536a962d955ef712a231
TomSamwan <114116342+TomSamwan@users.noreply.github.com>
2024-11-19 22:33:59 +0000

    Deploy Retry
    updated author ID to match previously registered "tom" user from seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,7 @@ const seedDB = async () => {
     const price = Math.floor(Math.random() * 20) + 10;
 
     const camp = new Campground({
-      author: '6724e8bedc30e11e1a1d3a06',
+      author: '673d10889dc350b1b0100618',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       geometry: {
