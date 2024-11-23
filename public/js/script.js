3ec1378bc0f8c2259b5ba475ed25648e4bbde11f
TomSamwan <114116342+TomSamwan@users.noreply.github.com>
2024-11-23 23:36:45 +0000

    Map control improvements & js warning cleanup
    Made it so that when you click the map overlay it disappears whilst you are using the map within it's container. when you click somewhere outside of the map container, the map overlay re-appears, disabling map controls
    
    fixed some js inline script errors by putting them into the script.js file instead of using inline 'onclick' functions as an example.
    
    fixed the dismiss button on the flash messages too

--- public/js/script.js
+++ public/js/script.js
@@ -1,93 +1,97 @@
-const ta = document.querySelectorAll('.form-input').forEach((el) => {
-
-  window.addEventListener('load', function () {
+const ta = document.querySelectorAll(".form-input").forEach((el) => {
+  window.addEventListener("load", function () {
     el.style.height = `${el.scrollHeight + 6}px`;
-  })
+  });
 
-  el.addEventListener('input', function () {
-    this.style.height = 'auto';
+  el.addEventListener("input", function () {
+    this.style.height = "auto";
     this.style.height = `${this.scrollHeight + 6}px`;
-  })
+  });
 
-  el.addEventListener('input', function () {
-    this.style.height = 'auto';
+  el.addEventListener("input", function () {
+    this.style.height = "auto";
     this.style.height = `${this.scrollHeight + 6}px`;
-  })
-
+  });
 });
 
-const price = document.querySelector('#price-input');
-const priceSpan = document.querySelector('.price-span')
+const price = document.querySelector("#price-input");
+const priceSpan = document.querySelector(".price-span");
 
-const inputPages = ['new', 'edit'];
-const path = window.location.pathname.toString().split('/');
+const inputPages = ["new", "edit"];
+const path = window.location.pathname.toString().split("/");
 const slug = path[path.length - 1];
 
 if (inputPages.includes(slug)) {
-  window.addEventListener('load', function () {
-    price.style.width = `${price.getAttribute('placeholder').length + 3}ch`
-  })
+  window.addEventListener("load", function () {
+    price.style.width = `${price.getAttribute("placeholder").length + 3}ch`;
+  });
 
-  price.addEventListener('input', function () {
+  price.addEventListener("input", function () {
     if (this.textLength == 0) {
-      this.style.width = `${this.getAttribute('placeholder').length + 3}ch`
+      this.style.width = `${this.getAttribute("placeholder").length + 3}ch`;
     } else {
       this.style.width = `${this.textLength + 3}ch`;
     }
-  })
+  });
 
-  price.addEventListener('focus', function () {
-    priceSpan.classList.add('active');
-    price.addEventListener('focusout', function () {
-      priceSpan.classList.remove('active');
-    })
-  })
+  price.addEventListener("focus", function () {
+    priceSpan.classList.add("active");
+    price.addEventListener("focusout", function () {
+      priceSpan.classList.remove("active");
+    });
+  });
 
-  document.querySelectorAll('.form-input').forEach((input) => {
-    input.addEventListener('focus', function () {
-      this.classList.add('active');
-      input.addEventListener('focusout', function () {
-        this.classList.remove('active');
-      })
-    })
-  })
+  document.querySelectorAll(".form-input").forEach((input) => {
+    input.addEventListener("focus", function () {
+      this.classList.add("active");
+      input.addEventListener("focusout", function () {
+        this.classList.remove("active");
+      });
+    });
+  });
 }
 
-const priceBtn = document.querySelector('.pay-button');
-priceBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-})
+// back button
+const prevPage = document.querySelector(".back-button");
+prevPage.addEventListener("click", () => {
+  window.history.back();
+});
 
-const flashMsg = document.querySelector('.flash-msg');
-function dismiss() {
-  flashMsg.style.display = "none";
+// flash messages
+
+const flashMsg = document.querySelector(".flash-msg");
+const flashDismiss = document.querySelector(".flash-dismiss");
+if (flashDismiss) {
+  flashDismiss.addEventListener("click", () => {
+    flashMsg.style.display = "none";
+  });
 }
 
 // Image carousel
 if (slug.length === 24) {
-  const carousel = document.querySelector('.slides');
-  const buttons = document.querySelectorAll('.carousel-button');
-  const slides = Array.from(document.querySelectorAll('.slide'));
+  // if slug length is 24, it means you're on the show page of a single campground
+  const carousel = document.querySelector(".slides");
+  const buttons = document.querySelectorAll(".carousel-button");
+  const slides = Array.from(document.querySelectorAll(".slide"));
 
   carousel.scrollTo(0, 0);
 
   if (slides.length > 1) {
-    const bubbles = document.querySelector('.bubbles');
+    const bubbles = document.querySelector(".bubbles");
     bubbles.innerHTML = `<div class="bubble"></div>`.repeat(slides.length); // add 'bubbles' based on 'slides.length'
 
-    document.querySelector('.slide').classList.add('currentSlide'); // sets 'active' class on first slide
-    document.querySelector('.bubble').classList.add('currentSlide'); // sets 'active' class on bubble element
+    document.querySelector(".slide").classList.add("currentSlide"); // sets 'active' class on first slide
+    document.querySelector(".bubble").classList.add("currentSlide"); // sets 'active' class on bubble element
 
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const offset = button.classList.contains("next") === true ? 1 : -1;
 
-    buttons.forEach(button => {
-      button.addEventListener('click', () => {
-        const offset = button.classList.contains('next') === true ? 1 : -1;
-
-        slides.forEach(slide => {
-          if (slide.classList.contains('currentSlide')) {
-            return activeSlide = slides.indexOf(slide)
+        slides.forEach((slide) => {
+          if (slide.classList.contains("currentSlide")) {
+            return (activeSlide = slides.indexOf(slide));
           }
-        })
+        });
 
         var newIndex = activeSlide + offset;
 
@@ -96,15 +100,21 @@ if (slug.length === 24) {
 
         carousel.scrollTo({
           top: 0,
-          left: (newIndex * 400),
-          behavior: 'smooth'
-        })
+          left: newIndex * 400,
+          behavior: "smooth",
+        });
 
-        slides[newIndex].classList.add('currentSlide');
-        slides[activeSlide].classList.remove('currentSlide');
-        bubbles.children[newIndex].classList.add('currentSlide');
-        bubbles.children[activeSlide].classList.remove('currentSlide');
-      })
-    })
+        slides[newIndex].classList.add("currentSlide");
+        slides[activeSlide].classList.remove("currentSlide");
+        bubbles.children[newIndex].classList.add("currentSlide");
+        bubbles.children[activeSlide].classList.remove("currentSlide");
+      });
+    });
   }
+
+  // dummy 'pay' button
+  const priceBtn = document.querySelector(".pay-button");
+  priceBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
 }
--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,17 +1,27 @@
 maptilersdk.config.apiKey = maptilerApiKey;
 const map = new maptilersdk.Map({
-    container: 'map',
-    style: maptilersdk.MapStyle.BRIGHT,
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
+  container: "single-map",
+  style: maptilersdk.MapStyle.BRIGHT,
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 10, // starting zoom
 });
 
 new maptilersdk.Marker()
-    .setLngLat(campground.geometry.coordinates)
-    .setPopup(
-        new maptilersdk.Popup({ offset: 40 })
-            .setHTML(
-                `<h3>${campground.title}</h3><p>${campground.location}</p>`
-            )
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new maptilersdk.Popup({ offset: 40 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
     )
-    .addTo(map)
+  )
+  .addTo(map);
+
+const singleMap = document.querySelector("#single-map");
+const mapOverlay = document.querySelector(".show-map-activate-overlay");
+
+window.addEventListener("mousedown", (e) => {
+  if (e.target === mapOverlay) {
+    mapOverlay.style.display = "none";
+  } else if (!e.target.getAttribute("class").includes("maplibregl")) {
+    mapOverlay.style.display = "flex";
+  }
+});
