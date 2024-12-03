19bc937cc1ee04b31cbf8743cc371594a3acd8b6
TomSamwan <114116342+TomSamwan@users.noreply.github.com>
2024-12-03 21:58:01 +0000

    Pagination functionality added
    Using the campgrounds controller middleware, and some code from online, I got pagination working with some simple styles and logic:
    - If you are on page 1, the previous button won't show.
    - if you're on the last page, the next button won't show.
    - if you are below page 1 (somehow) or above the last page, you get an error
    - if the page is between page 1 and the last page, both previous and next buttons show.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,83 +1,110 @@
-const Campground = require('../models/campground');
-const maptilerClient = require('@maptiler/client');
+const Campground = require("../models/campground");
+const maptilerClient = require("@maptiler/client");
 maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
-const { cloudinary } = require('../cloudinary')
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render('campgrounds/index', { campgrounds })
-}
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const startIndex = (page - 1) * limit;
+  const totalPages = await Campground.countDocuments() / limit;
+
+  const campgrounds = await Campground.find().skip(startIndex).limit(limit);
+
+  res.render("campgrounds/index", {
+    campgrounds,
+    page,
+    limit,
+    startIndex,
+    totalPages
+  });
+};
 
 module.exports.renderNewForm = (req, res) => {
-  res.render('campgrounds/new');
-}
+  res.render("campgrounds/new");
+};
 
 module.exports.createCampground = async (req, res) => {
-  const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+  const geoData = await maptilerClient.geocoding.forward(
+    req.body.campground.location,
+    { limit: 1 }
+  );
   const campground = new Campground(req.body.campground);
   campground.geometry = geoData.features[0].geometry;
-  campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+  campground.images = req.files.map((f) => ({
+    url: f.path,
+    filename: f.filename,
+  }));
   campground.author = req.user._id;
   await campground.save();
-  req.flash('success', 'Successfully made a new campground!');
-  res.redirect(`/campgrounds/${campground._id}`)
-}
-
+  req.flash("success", "Successfully made a new campground!");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
 
 module.exports.showCampground = async (req, res) => {
   const { id } = req.params;
   if (id.length !== 24) {
-    req.flash('error', 'Cannot find that campground');
-    return res.redirect('/campgrounds')
+    req.flash("error", "Cannot find that campground");
+    return res.redirect("/campgrounds");
   }
-  const campground = await Campground.findById(id).populate({
-    path: 'reviews',
-    populate: {
-      path: 'author'
-    }
-  }).populate('author');
+  const campground = await Campground.findById(id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("author");
   if (!campground) {
-    req.flash('error', 'Cannot find that campground');
-    return res.redirect('/campgrounds')
+    req.flash("error", "Cannot find that campground");
+    return res.redirect("/campgrounds");
   }
-  res.render('campgrounds/show', { campground });
-}
+  res.render("campgrounds/show", { campground });
+};
 
 module.exports.renderEditCampground = async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (id.length !== 24) {
-    req.flash('error', 'Cannot find that campground');
-    return res.redirect('/campgrounds')
+    req.flash("error", "Cannot find that campground");
+    return res.redirect("/campgrounds");
   }
   if (!campground) {
-    req.flash('error', 'Cannot find that campground');
-    return res.redirect('/campgrounds')
+    req.flash("error", "Cannot find that campground");
+    return res.redirect("/campgrounds");
   }
-  res.render('campgrounds/edit', { campground })
-}
+  res.render("campgrounds/edit", { campground });
+};
 
 module.exports.updateCampground = async (req, res) => {
   const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-  const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  const geoData = await maptilerClient.geocoding.forward(
+    req.body.campground.location,
+    { limit: 1 }
+  );
   campground.geometry = geoData.features[0].geometry;
-  const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.images.push(...imgs);
   await campground.save();
   if (req.body.deleteImages) {
-    for(let filename of req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
       await cloudinary.uploader.destroy(filename);
     }
-    await campground.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}})
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
   }
-  req.flash('success', 'Successfully updated campground!');
+  req.flash("success", "Successfully updated campground!");
   res.redirect(`/campgrounds/${campground._id}`);
-}
+};
 
 module.exports.deleteCampground = async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
-  req.flash('success', 'Successfully deleted campground!')
-  res.redirect('/campgrounds');
-}
+  req.flash("success", "Successfully deleted campground!");
+  res.redirect("/campgrounds");
+};
